4dde40715a372b8f40cb98ec25d70f56ac7931e5
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-05 21:07:42 +0800

    Update ATLANTIC.py

--- ATLANTIC.py
+++ ATLANTIC.py
@@ -2,8 +2,6 @@ import streamlit as st
 def BostonCeltics():
   st.header('Boston Celtics')
   st.write('Boston Celtics(1946年-至今)')
-  col1, col2,col3= st.columns(3)
-  col1.metric("冠軍🏆", "1  次")
-  col2.metric("聯盟冠軍🏆", "22  次")
-  col3.metric("分組冠軍🏆", "32  次")
-  st.write('Boston Celtics')
+  col1, col2= st.columns(2)
+  col1.metric("聯盟冠軍🏆", "22  次")
+  col2.metric("分組冠軍🏆", "32  次")
