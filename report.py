9e0f36e7f184b1f4700f1d7995ed5efcf081e767
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-06 01:17:49 +0800

    Update report.py

--- report.py
+++ report.py
@@ -1,8 +1,9 @@
 import streamlit as st  
 import ATLANTIC 
+import Central
 st.title('NBA數據查詢系統')
 st.sidebar.header('選擇球隊及數據')
-option = st.sidebar.selectbox( '選擇分組？', ['ATLANTIC', '中央組', '東南組', '西北組','太平洋組','西南組'])
+option = st.sidebar.selectbox( '選擇分組？', ['ATLANTIC', 'Central', '東南組', '西北組','太平洋組','西南組'])
 if option=='ATLANTIC':
   teams=st.sidebar.selectbox( '選擇球隊？', ['Boston Celtics', 'Brooklyn Nets', 'New York Knicks', 'Philadelphia 76ers','Toronto Raptors'])
   if teams=='Boston Celtics':
@@ -15,6 +16,17 @@ if option=='ATLANTIC':
     ATLANTIC.Philadelphia76ers() 
   if teams=='Toronto Raptors':
     ATLANTIC.TorontoRaptors()
-    
+if option=='Central':
+  teams=st.sidebar.selectbox( '選擇球隊？', ['Chicago Bulls', 'Cleveland Cavaliers', 'Detroit Pistons', 'Indiana Pacers','Milwaukee Bucks'])
+  if teams=='Chicago Bulls':
+    Central.ChicagoBulls()
+  if teams=='Cleveland Cavaliers':
+    Central.ClevelandCavaliers()
+  if teams=='Detroit Pistons':
+    Central.DetroitPistons()  
+  if teams=='Indiana Pacers':
+    Central.IndianaPacers() 
+  if teams=='Milwaukee Bucks':
+    Central.MilwaukeeBucks()
     
     
