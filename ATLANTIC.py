663e238666d7c9790a85c00bcd37d5efdd660443
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-05 21:27:15 +0800

    Update ATLANTIC.py

--- ATLANTIC.py
+++ ATLANTIC.py
@@ -2,9 +2,17 @@ import streamlit as st
 from PIL import Image  
 def BostonCeltics():
   st.header('Boston Celtics')
-  image = Image.open('Boston Celtics.png')
+  #image = Image.open('Boston Celtics.png')
   st.write('Boston Celtics(1946年-至今)')
   col1, col2= st.columns(2)
   col1.metric("聯盟冠軍🏆", "22  次")
   col2.metric("分組冠軍🏆", "32  次")
   st.write('波士頓塞爾蒂克隊的英文隊名為Boston Celtics，成立於1946年，目前所在地區是美國麻塞諸塞州波士頓市，主場為TD北岸花園球館，為美國職籃史上獲得總冠軍次數最多的球隊。')
+def BrooklynNets():
+  st.header('Brooklyn Nets')
+  #image = Image.open('Brooklyn Nets.png')
+  st.write('Brooklyn Nets(1946年-至今)')
+  col1, col2= st.columns(2)
+  col1.metric("聯盟冠軍🏆", "22  次")
+  col2.metric("分組冠軍🏆", "32  次")
+  st.write('　布魯克林籃網隊的英文隊名為Brooklyn Nets，球隊成立於1967年，目前所在城市是美國紐約州布魯克林(Brooklyn, New York)，主場為大陸航空中心體育館(Prudential Center)。球隊原名紐澤西籃網隊（New Jersey Nets），2012年球隊遷至紐約布魯克林，4月底更名為「布魯克林籃網隊（Brooklyn Nets）。')
