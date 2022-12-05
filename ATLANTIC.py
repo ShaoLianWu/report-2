66b12e2b59c150ad14c9cf5243fa5870f6191694
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-05 22:42:23 +0800

    Update ATLANTIC.py

--- ATLANTIC.py
+++ ATLANTIC.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image  
 def BostonCeltics():
   st.header('Boston Celtics')
-  #image = Image.open('Boston Celtics.png')
+  image = Image.open('BostonCeltics.png')
   st.write('Boston Celtics(1946年-至今)')
   col1, col2= st.columns(2)
   col1.metric("聯盟冠軍🏆", "22  次")
