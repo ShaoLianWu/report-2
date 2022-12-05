c998d025ac12812c7f57b32b3e670cc97958c783
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-05 23:05:29 +0800

    Update ATLANTIC.py

--- ATLANTIC.py
+++ ATLANTIC.py
@@ -1,17 +1,23 @@
 import streamlit as st  
 from PIL import Image  
 def BostonCeltics():
-  st.header('Boston Celtics')
-  st.write('Boston Celtics(1946年-至今)')
   col1, col2 = st.columns(2)
   with col1:
-   image = Image.open('BostonCeltics.png')
-   st.image(image)
+    image = Image.open('BostonCeltics.png')
+    st.image(image)
+  
+
+  
+  
   with col2:
-      st.write('波士頓塞爾蒂克隊的英文隊名為Boston Celtics，成立於1946年，目前所在地區是美國麻塞諸塞州波士頓市，主場為TD北岸花園球館，為美國職籃史上獲得總冠軍次數最多的球隊。')
-      col1, col2= st.columns(2)
-      col1.metric("聯盟冠軍🏆", "22  次")
-      col2.metric("分組冠軍🏆", "32  次")
+     
+     st.header('Boston Celtics')  
+     st.write('Boston Celtics(1946年-至今)')
+      
+  st.write('波士頓塞爾蒂克隊的英文隊名為Boston Celtics，成立於1946年，目前所在地區是美國麻塞諸塞州波士頓市，主場為TD北岸花園球館，為美國職籃史上獲得總冠軍次數最多的球隊。')
+  col1, col2= st.columns(2)
+  col1.metric("聯盟冠軍🏆", "22  次")
+  col2.metric("分組冠軍🏆", "32  次")   
 def BrooklynNets():
   st.header('Brooklyn Nets')
   #image = Image.open('Brooklyn Nets.png')
