d1319a99f5c5f15fc4a7ee975842226415f1ec88
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-05 22:45:49 +0800

    Update ATLANTIC.py

--- ATLANTIC.py
+++ ATLANTIC.py
@@ -3,6 +3,7 @@ from PIL import Image
 def BostonCeltics():
   st.header('Boston Celtics')
   image = Image.open('BostonCeltics.png')
+  st.image(image)
   st.write('Boston Celtics(1946年-至今)')
   col1, col2= st.columns(2)
   col1.metric("聯盟冠軍🏆", "22  次")
