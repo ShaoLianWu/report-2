0c383e4484b5890ec235f1e80f57ea4a86acfa4d
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-05 20:27:20 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import streamlit as st  
-import ATLANTIC
+import ATLANTIC as st 
 st.title('NBA數據查詢系統')
 st.sidebar.header('選擇球隊及數據')
 option = st.sidebar.selectbox( '選擇分組？', ['ATLANTIC', '中央組', '東南組', '西北組','太平洋組','西南組'])
