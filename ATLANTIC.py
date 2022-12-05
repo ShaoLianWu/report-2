6b608e4cbb961bc3fc34d4975089dc4f64db0068
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-06 00:11:05 +0800

    Update ATLANTIC.py

--- ATLANTIC.py
+++ ATLANTIC.py
@@ -13,8 +13,8 @@ def BostonCeltics():
   st.write('波士頓塞爾提克(1946年-至今)') 
   st.write('波士頓塞爾蒂克隊的英文隊名為Boston Celtics，成立於1946年，目前所在地區是美國麻塞諸塞州波士頓市，主場為TD北岸花園球館，為美國職籃史上獲得總冠軍次數最多的球隊。')
   col1, col2= st.columns(2)
-  col1.metric("聯盟冠軍🏆", "22  次")
-  col2.metric("分組冠軍🏆", "32  次")   
+  col1.metric("聯盟冠軍🏆", "17  次")
+  col2.metric("分組冠軍🏆", "22  次")   
 def BrooklynNets():
   st.header('Brooklyn Nets')
   image = Image.open('teams logo/BrooklynNets.png')
