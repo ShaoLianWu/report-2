664bbebf186f99b8b56a1167cdaf6fe662ed86cb
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-06 12:04:18 +0800

    Update Atlantic_map.py

--- Atlantic_map.py
+++ Atlantic_map.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import folium    
 def BostonCeltics_map():
+    col1, col2 = st.columns(2)
     with col1:
         st.header('主場:TD北岸花園球館')
         st.write('地址：臺中市北屯區崇德路三段835號')
@@ -8,7 +9,7 @@ def BostonCeltics_map():
         st.write('全壘打牆距離：左外野：325英呎 中外野：400英呎 右外野：325英呎')
         image = Image.open('台中洲際球場.png')
         st.image(image)
-      with col2:
+    with col2:
         m1 = folium.Map(location=[42.5869, 71.34402], zoom_start=16)
         # add marker for Liberty Bell
         tooltip = "臺中洲際棒球場"
