1ae7946db43d11e3079a55d909f86cf70b338bda
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-06 12:02:15 +0800

    Update report.py

--- report.py
+++ report.py
@@ -11,6 +11,7 @@ if option=='Atlantic':
   teams=st.sidebar.selectbox( '選擇球隊？', ['Boston Celtics', 'Brooklyn Nets', 'New York Knicks', 'Philadelphia 76ers','Toronto Raptors'])
   if teams=='Boston Celtics':
     Atlantic.BostonCeltics()
+    Atlantic_map.
   if teams=='Brooklyn Nets':
     Atlantic.BrooklynNets()
   if teams=='New York Knicks':
