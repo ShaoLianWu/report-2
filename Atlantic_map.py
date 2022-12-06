34208b76fc8107c3ae28d631ee9531f045c223e3
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-06 20:53:06 +0800

    Update Atlantic_map.py

--- Atlantic_map.py
+++ Atlantic_map.py
@@ -3,22 +3,23 @@ import folium
 from PIL import Image  
 from streamlit_folium import folium_static 
 def BostonCeltics_map():
+    TDGarden= folium.Map(location=[42.36622394101576, -71.06214665765047], zoom_start=16)
+        # add marker for Liberty Bell
+    tooltip = "TD花園"
+    folium.Marker([42.36622394101576, -71.06214665765047], popup="TD花園", tooltip=tooltip
+    ).add_to(TDGarden)
+    folium_static(TDGarden)
     col1, col2 = st.columns(2)
     with col1:
         st.header('主場:TD花園')
         st.write('地址：100 Legends Way, Boston, MA 02114美國')
         st.write('觀眾席數：18624席')
+        
+    with col2:
         image = Image.open('Home/TD Garden.jpg')
         image1 = Image.open('Home/TD Garden1.jpg')
         st.image(image)
         st.image(image1)
-    with col2:
-        TDGarden= folium.Map(location=[42.36622394101576, -71.06214665765047], zoom_start=16)
-        # add marker for Liberty Bell
-        tooltip = "TD花園"
-        folium.Marker([42.36622394101576, -71.06214665765047], popup="TD花園", tooltip=tooltip
-        ).add_to(TDGarden)
-        folium_static(TDGarden)
 def BrooklynNets_map():
     col1, col2 = st.columns(2)
     with col1:
