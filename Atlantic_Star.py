e3cd71182f506aab2bd49a6d19141544e484afcc
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-10 19:55:01 +0800

    Update Atlantic_Star.py

--- Atlantic_Star.py
+++ Atlantic_Star.py
@@ -1,5 +1,7 @@
 import streamlit as st  
 from PIL import Image
+image = Image.open('star/傳奇球星.jpg')
+st.image(image) 
 def BostonCeltics_Star():
   option=st.selectbox('選擇球星？',['Bill Russell', 'Larry Bird', 'Paul Pierce'])
 
