492e86be2c866693249e7ac764f7524d7af95117
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-10 21:25:06 +0800

    Update Atlantic_Star.py

--- Atlantic_Star.py
+++ Atlantic_Star.py
@@ -5,17 +5,17 @@ import xlrd
 import openpyxl
 def BostonCeltics_Star():
   st.header('Boston Celtics三大傳奇球星')
-  image = Image.open('star/傳奇球星.jpg')
-  st.image(image) 
   option=st.selectbox('選擇球星？',['Bill Russell', 'Larry Bird', 'Paul Pierce'])
   if option=='Bill Russell':
+    image = Image.open('star/guardians.jpg')
+    st.image(image) 
     df = pd.read_excel("star/Atlantic_Central_Star.xlsx",sheet_name="工作表1",usecols="A:H")
     new_df = df[0:1]
     st.dataframe(new_df)
-    try:
-      winnerimage = Image.open('star/guardians.png')
-      st.image(winnerimage)
-    except:
-      pass
+  else:
+      image = Image.open('star/傳奇球星.jpg')
+      st.image(image) 
+    
+   
       
 
