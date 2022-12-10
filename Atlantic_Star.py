670fa628b6221bdf9a439bcd41ba83dcdcd716dd
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-10 20:20:32 +0800

    Update Atlantic_Star.py

--- Atlantic_Star.py
+++ Atlantic_Star.py
@@ -2,11 +2,12 @@ import streamlit as st
 from PIL import Image
 import pandas as pd
 import xlrd  
+import openpyxl
 def BostonCeltics_Star():
   st.header('Boston Celtics三大傳奇球星')
   image = Image.open('star/傳奇球星.jpg')
   st.image(image) 
   option=st.selectbox('選擇球星？',['Bill Russell', 'Larry Bird', 'Paul Pierce'])
-  df = pd.read_excel("star/Atlantic_Central_Star.xlsx",nrows=[2:3])
+  df = pd.read_excel("star/Atlantic_Central_Star.xlsx")
   st.write(df)
 
