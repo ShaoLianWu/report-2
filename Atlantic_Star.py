f46acb6d218889f770ca035306bf7ab6c577bfa6
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-10 20:38:59 +0800

    Update Atlantic_Star.py

--- Atlantic_Star.py
+++ Atlantic_Star.py
@@ -8,6 +8,6 @@ def BostonCeltics_Star():
   image = Image.open('star/傳奇球星.jpg')
   st.image(image) 
   option=st.selectbox('選擇球星？',['Bill Russell', 'Larry Bird', 'Paul Pierce'])
-  df = pd.read_excel("star/Atlantic_Central_Star.xlsx",sheet_name="工作表1",usecols="球員,出賽,投籃命中率,三分命中率,罰球命中率,總籃板,總助攻,總得分")
+  df = pd.read_excel("star/Atlantic_Central_Star.xlsx",sheet_name="工作表1",usecols="A,B,C,D,E,F,G")
   st.write(df)
 
