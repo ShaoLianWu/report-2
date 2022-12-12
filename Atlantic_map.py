3763549f8af9ca902a5c6083e7bb83ebac9d1c22
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-12 19:29:27 +0800

    Update Atlantic_map.py

--- Atlantic_map.py
+++ Atlantic_map.py
@@ -3,18 +3,19 @@ import folium
 from PIL import Image  
 from streamlit_folium import folium_static 
 def BostonCeltics_map():
-    st.header('主場:TD花園')
-    TDGarden= folium.Map(location=[42.36622394101576, -71.06214665765047], zoom_start=16)
-        # add marker for Liberty Bell
-    tooltip = "TD花園"
-    folium.Marker([42.36622394101576, -71.06214665765047], popup="TD花園", tooltip=tooltip
-    ).add_to(TDGarden)
-    folium_static(TDGarden)
     col1, col2 = st.columns(2)
     with col1:
+        st.header('主場:TD花園')
+        TDGarden= folium.Map(location=[42.36622394101576, -71.06214665765047], zoom_start=16)
+            # add marker for Liberty Bell
+        tooltip = "TD花園"
+        folium.Marker([42.36622394101576, -71.06214665765047], popup="TD花園", tooltip=tooltip
+        ).add_to(TDGarden)
+        folium_static(TDGarden)
+    
+    with col2:
         image = Image.open('Home/TD Garden.jpg')
-        st.image(image)        
-    with col2:        
+        st.image(image)              
         image1 = Image.open('Home/TD Garden1.jpg')
         st.image(image1)
     st.write('地址：100 Legends Way, Boston, MA 02114美國,觀眾席數：18624席')
